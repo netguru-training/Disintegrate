feec46b63d2bf59fbe6fe32de6839b87d0155d36
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-08-29 12:26:43 +0200

    User decent exposure for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
+  expose(:users)
+  expose(:user)
   def index
-    @users = User.all
   end
 
   def show
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 end
