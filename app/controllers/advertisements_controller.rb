60a0b6a3d9a6c6fe683ae1c42c0efec8c8861d28
Pawel Wawrzyniak <sensei@wpsamurai.pl>
2015-08-29 10:07:10 +0000

    Add expose and create, update, destroy actions to AdvertisementsController

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,2 +1,32 @@
 class AdvertisementsController < ApplicationController
+  expose(:advertisement, attributes: :advertisements_params)
+  expose(:advertisements)
+
+  def create
+    if advertisement.save
+      redirect_to(advertisement)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if advertisement.save
+      redirect_to(advertisement)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    advertisement.destroy
+    redirect_to advertisements_path, notice: 'Advertisement deleted'
+  end
+
+  private
+
+  def advertisements_params
+    params.require(:advertisement).permit(:description, :title)
+  end
+
 end
