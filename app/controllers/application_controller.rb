b7b11f33a83f92b21a02516930ec9058aa2809d1
Pawel Wawrzyniak <sensei@wpsamurai.pl>
2015-08-29 10:18:31 +0000

    Fix conflicts on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -10,4 +15,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name << :description
   end
+
+
+
 end
