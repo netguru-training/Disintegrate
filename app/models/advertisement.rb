c34aaed7137ed44cfe8d8dc83220e489dc197ecc
Tomasz Jaśkiewicz <jasek2@gmail.com>
2015-08-30 11:23:02 +0200

    advertisement reference to comments

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,6 +1,7 @@
 class Advertisement < ActiveRecord::Base
 
   has_many :applications
+  has_many :comments
   belongs_to :user
 
   geocoded_by :address, :latitude  => :map_lat, :longitude => :map_long
