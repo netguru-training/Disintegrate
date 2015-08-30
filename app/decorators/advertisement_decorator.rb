a4d92fc08c2f7d2d8ae8a9df923d2a9e8d960d04
Pawel Wawrzyniak <sensei@wpsamurai.pl>
2015-08-30 10:03:18 +0000

    Fix advertisement decorator for empty dates

--- app/decorators/advertisement_decorator.rb
+++ app/decorators/advertisement_decorator.rb
@@ -2,6 +2,7 @@ class AdvertisementDecorator < ApplicationDecorator
 
   %w(start_date end_date).each do |method|
     define_method method do
+      return if object.public_send(method).nil?
       object.public_send(method).strftime('%d-%m-%Y')
     end
   end
